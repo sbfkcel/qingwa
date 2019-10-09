ee6697344a312d3376f46ba98cfaa71d7582c8dd
单炒饭 <sbfkcel@163.com>
2019-10-09 15:10:14 +0800

    移除不必要的try catch

--- main.js
+++ main.js
@@ -17,19 +17,16 @@ const frog = ()=>{
         return `\u001B[${color[0]}m${text}\u001B[${color[1]}m`;
     },
     getText = ()=>{
-        try {
-            throw new Error('');
-        } catch (err) {
-            let strErr = err.stack,
-                strLineErr = strErr.split(/\r|\n/)[3],
-                arrErrResult = strLineErr.match(/[^/|:|\\]{1,}/ig),
-                temp = {};
-            // _console.log(strLineErr)
-            temp.colNum = +arrErrResult.pop();
-            temp.lineNum = +arrErrResult.pop();
-            temp.fileName = arrErrResult.pop();
-            return /\\|\//ig.test(strLineErr) ? `🐸 ${getStyle('magenta',temp.fileName)}${getStyle('gray',':')}${getStyle('green',temp.lineNum)}` : undefined;
-        };
+        let err = new Error(),
+            strErr = err.stack,
+            strLineErr = strErr.split(/\r|\n/)[3],
+            arrErrResult = strLineErr.match(/[^/|:|\\]{1,}/ig),
+            temp = {};
+        // _console.log(strLineErr)
+        temp.colNum = +arrErrResult.pop();
+        temp.lineNum = +arrErrResult.pop();
+        temp.fileName = arrErrResult.pop();
+        return /\\|\//ig.test(strLineErr) ? `🐸 ${getStyle('magenta',temp.fileName)}${getStyle('gray',':')}${getStyle('green',temp.lineNum)}` : undefined;
     };
     
     // cover console
